2dbd27e29f369e2f348e2f7b406590fe3196fae4
viti95 <victornt95@gmail.com>
2022-07-29 09:48:52 +0200

    Better precision for fast inverse square root

--- src/engine/math_util.c
+++ src/engine/math_util.c
@@ -129,7 +129,7 @@ static inline float Q_rsqrt( float number )
     x2 = number * 0.5F;
     y  = number;
     i  = * ( long * ) &y;                       // evil floating point bit level hacking
-    i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
+    i  = 0x5f375a86 - ( i >> 1 );               // what the fuck?
     y  = * ( float * ) &i;
     y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
 
